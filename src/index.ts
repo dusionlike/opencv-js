e19670a937f1c244ba821d60799646ac3b8edfa4
zhangzhixiong <751069244@qq.com>
2024-12-31 17:52:20 +0800

    fix: support namespace types

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,5 @@
-import { CV } from "./types/opencv";
-import createOpenCv from "../lib/opencv_js";
+export * from "./types/opencv";
 
-type CV2 = CV & {
-  loadOpenCV: (wasmFile: string | ArrayBuffer) => Promise<void>;
-  loadPromise?: Promise<void>;
-};
+export declare const loadPromise: Promise<void> | undefined;
 
-const cv = {
-  async loadOpenCV(wasmFile) {
-    if (!this.loadPromise) {
-      this.loadPromise = (async () => {
-        let __cv: any;
-        if (typeof wasmFile === "string") {
-          __cv = await createOpenCv({
-            locateFile: () => wasmFile,
-          });
-        } else {
-          __cv = await createOpenCv({
-            wasmBinary: wasmFile,
-          });
-        }
-        Object.assign(this, __cv);
-      })();
-    }
-    return this.loadPromise;
-  },
-} as CV2;
-
-export default cv;
+export declare function loadOpenCV(wasmFile: string | ArrayBuffer): Promise<void>;
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: {
+    'index': 'src/opencv.ts',
+  },
   outDir: 'dist',
   format: ['cjs', 'esm'],
 })
