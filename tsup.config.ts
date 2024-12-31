24bcbdacbb23e3e96362fb2492534da1d6b7a4a2
zhangzhixiong <751069244@qq.com>
2024-12-31 18:46:23 +0800

    feat: add exports field and browser-specific configuration in tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,29 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig({
-  entry: {
-    'index': 'src/opencv.ts',
+export default defineConfig([
+  {
+    entry: {
+      'index': 'src/opencv.ts',
+    },
+    outDir: 'dist',
+    format: ['cjs', 'esm'],
   },
-  outDir: 'dist',
-  format: ['cjs', 'esm'],
-})
+  {
+    entry: {
+      'index': 'src/opencv.ts',
+    },
+    outDir: 'dist',
+    format: ['cjs', 'esm'],
+    define: {
+      'require': 'undefined',
+      'process': 'undefined',
+    },
+    platform: 'browser',
+    treeshake: true,
+    outExtension(ctx) {
+      return {
+        js: ctx.format === 'esm' ? '.web.mjs' : '.web.js',
+      }
+    },
+  },
+])
